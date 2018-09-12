518be917d7351f281e84643a70effd7bbcbbbc95
Tiago Martinho <t.martinho@icloud.com>
2018-09-12 19:03:23 +0200

    Implement previous

--- core/src/main/java/com/tm/core/PlayerPresenter.kt
+++ core/src/main/java/com/tm/core/PlayerPresenter.kt
@@ -5,8 +5,27 @@ class PlayerPresenter(
         val view: PlayerView
 ) {
 
+    private var currentChannelIndex = 0
+
     fun play() {
         if (channels.isEmpty()) { return }
-        view.play(channels.first())
+        if (currentChannelIndex >= channels.count()) { return }
+        view.play(channels[currentChannelIndex])
+    }
+
+    fun next() {
+        if (channels.isEmpty()) { return }
+        currentChannelIndex += 1
+        currentChannelIndex /= channels.count()
+        play()
+    }
+
+    fun previous() {
+        if (channels.isEmpty()) { return }
+        currentChannelIndex -= 1
+        if (currentChannelIndex < 0) {
+            currentChannelIndex = 0
+        }
+        play()
     }
 }
--- core/src/test/java/com/tm/core/PlayerPresenterTest.kt
+++ core/src/test/java/com/tm/core/PlayerPresenterTest.kt
@@ -3,6 +3,7 @@ package com.tm.core
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import org.mockito.internal.verification.VerificationModeFactory.times
 
 class PlayerPresenterTest {
 
@@ -25,6 +26,60 @@ class PlayerPresenterTest {
 
         presenter.play()
 
-        verify(view).play(channel = first)
+        verify(view, times(1)).play(channel = first)
+    }
+
+    @Test
+    fun `play next channel in list`() {
+        val first = Channel("firstName", "firstURL")
+        val second = Channel("secondName", "secondURL")
+        val channels = arrayListOf(first, second)
+        val presenter = PlayerPresenter(channels, view)
+        presenter.play()
+
+        presenter.next()
+
+        verify(view, times(2)).play(channel = first)
+    }
+
+    @Test
+    fun `play next channel loops list`() {
+        val first = Channel("firstName", "firstURL")
+        val second = Channel("secondName", "secondURL")
+        val channels = arrayListOf(first, second)
+        val presenter = PlayerPresenter(channels, view)
+        presenter.play()
+        presenter.next()
+
+        presenter.next()
+
+        verify(view, times(3)).play(channel = first)
+    }
+
+    @Test
+    fun `play previous channel in list`() {
+        val first = Channel("firstName", "firstURL")
+        val second = Channel("secondName", "secondURL")
+        val channels = arrayListOf(first, second)
+        val presenter = PlayerPresenter(channels, view)
+        presenter.play()
+
+        presenter.previous()
+
+        verify(view, times(2)).play(channel = first)
+    }
+
+    @Test
+    fun `play previous channel loops list`() {
+        val first = Channel("firstName", "firstURL")
+        val second = Channel("secondName", "secondURL")
+        val channels = arrayListOf(first, second)
+        val presenter = PlayerPresenter(channels, view)
+        presenter.play()
+        presenter.previous()
+
+        presenter.previous()
+
+        verify(view, times(3)).play(channel = first)
     }
 }
