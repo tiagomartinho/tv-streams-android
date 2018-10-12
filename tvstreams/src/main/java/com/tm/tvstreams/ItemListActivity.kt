6bd9f902cc85c03e71528bb76dcfa4f2b79d7539
Tiago Martinho <t.martinho@icloud.com>
2018-10-12 11:57:03 +0200

    Go full screen when selecting channel

--- tvstreams/src/main/java/com/tm/tvstreams/ItemListActivity.kt
+++ tvstreams/src/main/java/com/tm/tvstreams/ItemListActivity.kt
@@ -1,10 +1,12 @@
 package com.tm.tvstreams
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.View.GONE
 import android.widget.Toast
 import channels.Channel
 import kotlinx.android.synthetic.main.activity_item_list.*
@@ -31,8 +33,10 @@ class ItemListActivity : AppCompatActivity(), ChannelListFragment.OnListFragment
                 return
             }
             channelListFragment = ChannelListFragment.newInstance()
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, channelListFragment).commit()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, channelListFragment)
+                .commit()
         }
         val userID = SharedPreferencesUserRepository(this).load().id
         val channelRepository = userID?.let { FireStoreChannelRepository(it) }
@@ -42,8 +46,10 @@ class ItemListActivity : AppCompatActivity(), ChannelListFragment.OnListFragment
     }
 
     override fun onListFragmentInteraction(channel: Channel?) {
-        Toast.makeText(applicationContext,"name", Toast.LENGTH_SHORT).show()
+        Toast.makeText(applicationContext, "name", Toast.LENGTH_SHORT).show()
         if (twoPane) {
+            fragment_container.visibility = GONE
+            fab.visibility = GONE
             val fragment = ItemDetailFragment().apply {
                 arguments = Bundle().apply {
                     putString(ItemDetailFragment.ARG_ITEM_ID, channel?.name)
@@ -53,11 +59,43 @@ class ItemListActivity : AppCompatActivity(), ChannelListFragment.OnListFragment
                 .beginTransaction()
                 .replace(R.id.item_detail_container, fragment)
                 .commit()
+            goFullscreen()
         } else {
             val intent = Intent(this, ItemDetailActivity::class.java).apply {
                 putExtra(ItemDetailFragment.ARG_ITEM_ID, channel?.name)
             }
             this.startActivity(intent)
         }
+    }
+
+    private fun goFullscreen() {
+        supportActionBar?.hide()
+        setUiFlags(true)
+    }
+
+    private fun exitFullscreen() {
+        supportActionBar?.show()
+        setUiFlags(false)
+    }
+
+    private fun setUiFlags(fullscreen: Boolean) {
+        window.decorView.systemUiVisibility = if (fullscreen) getFullscreenUiFlags() else getStableUiFlags()
+    }
+
+    private fun getFullscreenUiFlags(): Int {
+        return (View.SYSTEM_UI_FLAG_LOW_PROFILE
+            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_IMMERSIVE
+            or View.GONE)
+    }
+
+    private fun getStableUiFlags(): Int {
+        return (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
     }
 }
