8c72e02915fc440dda9f1135f7d594553ef430f8
Tiago Martinho <t.martinho@icloud.com>
2018-10-12 10:42:14 +0200

    Use ChannelListFragment

--- tvstreams/src/main/java/com/tm/tvstreams/ItemListActivity.kt
+++ tvstreams/src/main/java/com/tm/tvstreams/ItemListActivity.kt
@@ -2,23 +2,19 @@ package com.tm.tvstreams
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.RecyclerView
 import android.support.design.widget.Snackbar
-import android.support.v7.widget.DividerItemDecoration
-import android.view.LayoutInflater
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-
-import com.tm.tvstreams.dummy.DummyContent
+import android.widget.Toast
+import channels.Channel
 import kotlinx.android.synthetic.main.activity_item_list.*
-import kotlinx.android.synthetic.main.item_list_content.view.*
 import kotlinx.android.synthetic.main.item_list.*
+import user.SharedPreferencesUserRepository
 
-class ItemListActivity : AppCompatActivity() {
+class ItemListActivity : AppCompatActivity(), ChannelListFragment.OnListFragmentInteractionListener {
 
     private var twoPane: Boolean = false
+    private lateinit var channelListFragment: ChannelListFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,65 +26,38 @@ class ItemListActivity : AppCompatActivity() {
                 .setAction("Action", null).show()
         }
         twoPane = item_detail_container != null
-        setupRecyclerView(item_list)
+        if (findViewById<View>(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return
+            }
+            channelListFragment = ChannelListFragment.newInstance()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, channelListFragment).commit()
+        }
+        val userID = SharedPreferencesUserRepository(this).load().id
+        val channelRepository = userID?.let { FireStoreChannelRepository(it) }
+        channelRepository?.channels {
+            channelListFragment.set(it)
+        }
     }
 
-    private fun setupRecyclerView(recyclerView: RecyclerView) {
-        recyclerView.adapter = SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, twoPane)
-        val decoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
-        recyclerView.addItemDecoration(decoration)
-    }
-
-    class SimpleItemRecyclerViewAdapter(
-        private val parentActivity: ItemListActivity,
-        private val values: List<DummyContent.DummyItem>,
-        private val twoPane: Boolean
-    ) :
-        RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder>() {
-
-        private val onClickListener: View.OnClickListener
-
-        init {
-            onClickListener = View.OnClickListener { v ->
-                val item = v.tag as DummyContent.DummyItem
-                if (twoPane) {
-                    val fragment = ItemDetailFragment().apply {
-                        arguments = Bundle().apply {
-                            putString(ItemDetailFragment.ARG_ITEM_ID, item.id)
-                        }
-                    }
-                    parentActivity.supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.item_detail_container, fragment)
-                        .commit()
-                } else {
-                    val intent = Intent(v.context, ItemDetailActivity::class.java).apply {
-                        putExtra(ItemDetailFragment.ARG_ITEM_ID, item.id)
-                    }
-                    v.context.startActivity(intent)
+    override fun onListFragmentInteraction(channel: Channel?) {
+        Toast.makeText(applicationContext,"name", Toast.LENGTH_SHORT).show()
+        if (twoPane) {
+            val fragment = ItemDetailFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ItemDetailFragment.ARG_ITEM_ID, channel?.name)
                 }
             }
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_list_content, parent, false)
-            return ViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val item = values[position]
-            holder.contentView.text = item.content
-            with(holder.itemView) {
-                tag = item
-                setOnClickListener(onClickListener)
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.item_detail_container, fragment)
+                .commit()
+        } else {
+            val intent = Intent(this, ItemDetailActivity::class.java).apply {
+                putExtra(ItemDetailFragment.ARG_ITEM_ID, channel?.name)
             }
-        }
-
-        override fun getItemCount() = values.size
-
-        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            val contentView: TextView = view.textView
+            this.startActivity(intent)
         }
     }
 }
