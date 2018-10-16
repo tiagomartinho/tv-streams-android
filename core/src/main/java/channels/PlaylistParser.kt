b5198f3c7e9c9bc892fa285c4f9b9a4423778d99
Tiago Martinho <t.martinho@icloud.com>
2018-10-16 11:58:57 +0200

    add csv parsing

--- core/src/main/java/channels/PlaylistParser.kt
+++ core/src/main/java/channels/PlaylistParser.kt
@@ -3,19 +3,35 @@ package channels
 class PlaylistParser {
     companion object {
         fun parse(content: String): ArrayList<Channel> {
-            var channels =  arrayListOf<Channel>()
+            var channels = arrayListOf<Channel>()
             var name: String? = null
-            for(line in content.lines()) {
-                if(NameParser.isChannel(line)) {
-                    name = NameParser.extract(line)
-                }
-                if(LinkParser.isLinkValid(line)){
-                    val link = LinkParser.extractLink(line)
-                    channels.add(Channel("a","b",name ?: link, link))
-                    name = null
+            for (line in content.lines()) {
+                if (isCommaSeparated(line)) {
+                    val splitLine = line.split(",")
+                    val streamName = NameParser.extract(splitLine.first())
+                    val link = LinkParser.extractLink(splitLine.last())
+                    channels.add(Channel("a", "b", streamName, link))
+                } else {
+                    if (NameParser.isChannel(line)) {
+                        name = NameParser.extract(line)
+                    }
+                    if (LinkParser.isLinkValid(line)) {
+                        val link = LinkParser.extractLink(line)
+                        channels.add(Channel("a", "b", name ?: link, link))
+                        name = null
+                    }
                 }
             }
             return channels
+        }
+
+        private fun isCommaSeparated(line: String): Boolean {
+            val splitLine = line.split(",")
+            return splitLine.first() != null &&
+                splitLine.last() != null &&
+                splitLine.count() == 2 &&
+                LinkParser.isLinkValid(splitLine.last()) &&
+                !line.startsWith("#")
         }
     }
 }
--- core/src/test/java/channels/PlaylistParserTest.kt
+++ core/src/test/java/channels/PlaylistParserTest.kt
@@ -5,22 +5,35 @@ import org.junit.Test
 
 class PlaylistParserTest {
 
-    val secondLink = "http://wownet.ro/str/0500.m3u8"
-    val secondName = "RTP 1"
-    val thirdLink = "https://9nowlivehls-i.akamaihd.net/hls/live/226644/ch9melprd/master.m3u8"
-    val thirdName = "Channel9Mel"
-    val lastLink = "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
-    
-    @Test fun extract_all_channels_from_list() {
+    @Test
+    fun extract_all_channels_from_list() {
+        val secondLink = "http://wownet.ro/str/0500.m3u8"
+        val secondName = "RTP 1"
+        val thirdLink = "https://9nowlivehls-i.akamaihd.net/hls/live/226644/ch9melprd/master.m3u8"
+        val thirdName = "Channel9Mel"
+        val lastLink = "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
         val content = PlaylistParserTest::class.java.getResource("/list.m3u").readText()
 
         val channels = PlaylistParser.parse(content)
 
         assertEquals(10, channels.count())
-        assertEquals(secondLink,channels[1].link)
-        assertEquals(secondName,channels[1].name)
-        assertEquals(thirdLink,channels[2].link)
-        assertEquals(thirdName,channels[2].name)
-        assertEquals(lastLink,channels.last().link)
+        assertEquals(secondLink, channels[1].link)
+        assertEquals(secondName, channels[1].name)
+        assertEquals(thirdLink, channels[2].link)
+        assertEquals(thirdName, channels[2].name)
+        assertEquals(lastLink, channels.last().link)
+    }
+
+    @Test
+    fun extract_from_csv_list() {
+        val content = PlaylistParserTest::class.java.getResource("/csv.m3u").readText()
+
+        val channels = PlaylistParser.parse(content)
+
+        assertEquals(2, channels.count())
+        assertEquals("http://02e4.vp9.tv/chn/btsu1/v.m3u8", channels[0].link)
+        assertEquals("BT SPORT 1 HD", channels[0].name)
+        assertEquals("http://203.162.121.230:1935/live/model_channel/index.m3u8?securetoken=#ed%h0#w@1", channels[1].link)
+        assertEquals("MODEL", channels[1].name)
     }
 }
