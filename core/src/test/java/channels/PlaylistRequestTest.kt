dd3d3863e14b39a9178f960549bfe9990c859277
Tiago Martinho <t.martinho@icloud.com>
2018-10-16 13:17:50 +0200

    Fix comparison

--- core/src/test/java/channels/PlaylistRequestTest.kt
+++ core/src/test/java/channels/PlaylistRequestTest.kt
@@ -4,21 +4,19 @@ import junit.framework.Assert.assertEquals
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.junit.Test
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
 
 class PlaylistRequestTest {
 
     @Test
     fun request() {
-        val expected = PlaylistParserTest::class.java.getResource("/remote.m3u").readText()
+        val expected = PlaylistParserTest::class.java.getResource("/remote.m3u").readText().replace("\n", "")
         val url = "https://pastebin.com/raw/a6dMQ8n5"
         val client = OkHttpClient()
         val request = Request.Builder()
             .url(url)
             .build()
         val response = client.newCall(request).execute()
-        val result = response.body()?.string() ?: ""
-        assertEquals(expected.substring(0,500), result.substring(0,500))
+        val result = response.body()?.string()?.replace("\r\n", "") ?: ""
+        assertEquals(expected,result)
     }
 }
